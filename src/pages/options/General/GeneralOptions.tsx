b8054b201a4abfe2ec0df1b8c3c49ad3c1fa574c
arsanbite <101806836+arsanbite@users.noreply.github.com>
2023-09-20 18:34:31 +0200

    Removed innecesary elements, added mantine library and started the options panel

---
+++ src/pages/options/General/GeneralOptions.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GeneralOptions: React.FC = () => {
+
+    return (
+        <div>
+            General options
+        </div>);
+};
+
+export default GeneralOptions;
